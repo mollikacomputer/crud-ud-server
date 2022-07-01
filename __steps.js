c3ac50568ecaa2c75c0748d1798e81716b461aa4
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-01 09:28:36 +0600

    create home route to test connection port 5000

---
+++ __steps.js
@@ -0,0 +1,13 @@
+/**
+ * --------------------------
+ * for server
+ * --------------------------
+ * 1. mkdir folder name or manually create server folder
+ * 2. cd space folder name hit enter to enter project folder
+ * 3. npm init
+ * 4. create index.js side of packagejson folder
+ * 5. npm install express
+ * 6. npm install nodemon globally
+ * 7. install cors and require import
+ * 8. require express
+ */
